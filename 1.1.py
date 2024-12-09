41c550a03abca6bb403da75c16834c89d412a7ff
Шепелюк Кирилл <ShepelyukK@lcoast.itstep>
2024-12-09 20:34:46 +0500

    comment

---
+++ 1.1.py
@@ -0,0 +1,59 @@
+# task 1
+# start = int(input("Начало"))
+# end = int(input("Конец"))
+
+# while start <= end:
+#     print(start)
+#     start += 1
+
+
+# task 2
+# start = int(input("Начало "))
+# end = int(input("Конец "))
+
+# while start <= end:
+#     if start % 2 != 0:
+#         print(start)
+#     start += 1
+
+
+# task 3
+# start = int(input("Начало "))
+# end = int(input("Конец "))
+
+# while start <= end:
+#     if start % 2 == 0:
+#         print(start)
+#     start += 1
+
+
+# task 4
+# start = int(input("Начало "))
+# end = int(input("Конец "))
+
+# if start > end:
+#     larger = start
+#     smaller = end
+# else:
+#     larger = end
+#     smaller = start
+
+# while larger >= smaller:
+#     print(larger)
+#     larger -= 1
+
+
+# task 5
+# start = int(input("Начало: "))
+# end = int(input("Конец: "))
+
+# if start > end:
+#     temp = start
+#     start = end
+#     end = temp
+
+# print("нечетные числа в диапазоне от", start, "до", end)
+# while start <= end:
+#     if start % 2 != 0:
+#         print(start)
+#     start += 1
